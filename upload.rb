e7b2284e6cdce7297add1994ea2d1f9366bd61e1
shimobayashi <shimobayashi@hatena.ne.jp>
2013-06-28 19:54:33 +0900

    インデントしたりとかなんかやった

--- upload.rb
+++ upload.rb
@@ -4,16 +4,16 @@ require 'digest/md5'
 
 begin
   cgi = CGI.new
-  
+
   file = cgi.params['file'][0]
   ext = File.extname(file.original_filename)
   body = file.read
   basename = Digest::MD5.hexdigest(body)
   filename = basename + ext
-  
+
   raise 'too large file' if body.size > 20000000
   raise 'not allowed ext' unless ['.mp3', '.aac', '.wav', '.ogg', '.m4a'].include?(ext)
-  
+
   open(Dir.getwd + '/music/' + filename, 'w') do |fh|
     fh.binmode
     fh.write body
@@ -21,7 +21,7 @@ begin
   open(Dir.getwd + '/current.json', 'w') do |fh|
     fh.write "{\"filename\":\"#{filename}\"}\n"
   end
-  
+
   print cgi.header("status" => "OK")
   print 'ok'
 rescue Exception => e
